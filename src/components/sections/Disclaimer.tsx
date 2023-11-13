94b0b6e786e948b8b893d75cc31d92dead3002f5
Hugo Hsi <hugohsi@pop-os.localdomain>
2023-11-13 15:04:13 -0500

    restyled disclaimer section

--- src/components/sections/Disclaimer.tsx
+++ src/components/sections/Disclaimer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Disclaimer() {
   return (
-    <div className="bg-neutral-200 p-6">
-      <h2 className="font-medium text-center">
+    <div className="bg-secondary p-4">
+      <h2 className="text-xs font-medium text-center">
         This project is not meant to be the answer to everything frontend
       </h2>
     </div>
