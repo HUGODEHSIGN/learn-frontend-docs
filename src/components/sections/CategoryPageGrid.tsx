3648e5b79b4ade1a5e6bc4b5fc5ed0769fe594d6
Hugo Hsi <hugohsi@pop-os.localdomain>
2023-11-13 15:16:54 -0500

    fixed key issues on footers and cards

--- src/components/sections/CategoryPageGrid.tsx
+++ src/components/sections/CategoryPageGrid.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import PageCard from '../multiUse/PageCard'
+import React from "react";
+import PageCard from "../multiUse/PageCard";
 
-import pageData from "../../app/pageData.json" assert { type: 'json'};
+import pageData from "../../app/pageData.json" assert { type: "json" };
 
 interface Props {
-    category: number
+  category: number;
 }
 
 export default function CategoryPageGrid({ category }: Props) {
-    const pages = pageData.categories[category].pages;
+  const pages = pageData.categories[category].pages;
 
-    const renderCards: () => any = () => {
-        return pages.map(({ page, description }) => (
-<PageCard title={ page } description={ description } link={`${pageData.categories[category].category.toLowerCase()}/${page.toLowerCase()}`}></PageCard>
-        ))
-    }
+  const renderCards: () => any = () => {
+    return pages.map(({ page, description, link }) => (
+      <PageCard
+        key={page}
+        title={page}
+        description={description}
+        link={link}
+      ></PageCard>
+    ));
+  };
 
-  return (
-    <div className='grid grid-cols-1 gap-4 p-6'>
-        {renderCards()}
-    </div>
-  )
+  return <div className="grid grid-cols-1 gap-4 p-6">{renderCards()}</div>;
 }
--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,36 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import pageData from '../../app/pageData.json'
-import { Button } from '../ui/button';
-import Link from 'next/link';
+import pageData from "../../app/pageData.json";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function Footer() {
-    const categories = pageData.categories;
+  const categories = pageData.categories;
 
-    const renderFooter: () => any = () => {
-        return categories.map(({ category, pages, link }) => (
-            <div>
-                <Button variant='link' className='text-lg font-medium' asChild>
-                    <Link href={ link }>
-                    { category }
-                    </Link>
-                    </Button>
-                <div className='flex flex-col items-start'>
-                    {pages.map(({ page, link }) => (
-                        <Button variant='link'>
-                            <Link href={ link }>
-                            { page }
-                            </Link>
-                            </Button>
-                    ))}
-                </div>
-            </div>
-        ))
-    }
+  const renderFooter: () => any = () => {
+    return categories.map(({ category, pages, link }) => (
+      <div key={category}>
+        <Button variant="link" className="text-lg font-medium" asChild>
+          <Link href={link}>{category}</Link>
+        </Button>
+        <div className="flex flex-col items-start">
+          {pages.map(({ page, link }) => (
+            <Button key={page} variant="link">
+              <Link href={link}>{page}</Link>
+            </Button>
+          ))}
+        </div>
+      </div>
+    ));
+  };
 
   return (
-    <div className='grid grid-cols-1 bg-secondary w-full p-8 gap-6'>
-        {renderFooter()}
+    <div className="grid grid-cols-1 bg-secondary w-full p-8 gap-6">
+      {renderFooter()}
     </div>
-  )
+  );
 }
